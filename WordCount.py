09a26533978d031d7fbd16c1ec78394f42351a4f
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-11-28 15:59:34 +0800

    Excluded Non-alphabet Characters, Added Context

--- WordCount.py
+++ WordCount.py
@@ -1,3 +1,14 @@
+#---------- CONTEXT --------------
+#Program 1:
+#Create a program that ask for a sentence as input.
+#Display the number of words, vowels and consonants in the input 
+#  Example:
+#     input: Sample Text here
+#     output:
+#           words: 3
+#           vowels: 6
+#           consonants: 8
+
 Usr_Text = input("Enter text: ").lower()
 
 vowel_list = "a","e","i","o","u"
@@ -10,11 +21,11 @@ for char in Usr_Text:
         word_count += 1
     elif char in vowel_list:
         vowels += 1
-    else:
-        if char.isdecimal() == False:
+    elif char not in vowel_list:
+        if char.isalpha() == True:
             consonants += 1
-        else:
-            None
+    else:
+        None
 
 print(word_count)
 print(vowels)
