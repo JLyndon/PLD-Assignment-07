e7748d42626529d2268c2b25a53c889dd88e35e6
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-11-28 17:37:37 +0800

    Added Instructions for Criteria C and D

--- PasswordValidator.py
+++ PasswordValidator.py
@@ -12,17 +12,27 @@
 
 Criteria_00 = False
 Criteria_02 = False
+Criteria_03 = False
+Criteria_04 = False
+
+spcl_Char = "'[@_!#$%^&*()<>?/\|}{~:]`+=-.,:;~"
 
 Usr_Pass = input("Password: ")
-if len(Usr_Pass) <= 14:
+if len(Usr_Pass) <= 14: # Initial Requirement
     print("Password must be at least 15 characters long!")
 else:
-    for PWChar in Usr_Pass:
+    for PWChar in Usr_Pass: # Iterate every char in PWString then evaluate.
         if PWChar.isalpha() == True: 
             if PWChar == PWChar.upper():
                 Criteria_02 = True
             elif PWChar == PWChar.lower():
                 Criteria_00 = True
+        elif PWChar.isdecimal() == True:
+            Criteria_03 = True
+        elif PWChar in spcl_Char:
+            Criteria_04 = False
 
-print(Criteria_02)       
-print(Criteria_00)
+print(Criteria_00)       
+print(Criteria_02)
+print(Criteria_03)
+print(Criteria_04)
