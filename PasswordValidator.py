4f2c2a99c117faea50c53c188f008b67392da178
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-11-28 17:49:33 +0800

    Combined All Criteria in One Loop

--- PasswordValidator.py
+++ PasswordValidator.py
@@ -11,6 +11,7 @@
 #   ouput: Valid
 
 Criteria_00 = False
+Criteria_01 = False
 Criteria_02 = False
 Criteria_03 = False
 Criteria_04 = False
@@ -18,21 +19,24 @@ Criteria_04 = False
 spcl_Char = "'[@_!#$%^&*()<>?/\|}{~:]`+=-.,:;~"
 
 Usr_Pass = input("Password: ")
-if len(Usr_Pass) <= 14: # Initial Requirement
-    print("Password must be at least 15 characters long!")
-else:
-    for PWChar in Usr_Pass: # Iterate every char in PWString then evaluate.
-        if PWChar.isalpha() == True: 
-            if PWChar == PWChar.upper():
-                Criteria_02 = True
-            elif PWChar == PWChar.lower():
-                Criteria_00 = True
-        elif PWChar.isdecimal() == True:
-            Criteria_03 = True
-        elif PWChar in spcl_Char:
-            Criteria_04 = False
+
+for PWChar in Usr_Pass: # Iterate every char in PWString then evaluate.
+    if PWChar.isalpha() == True: 
+        if PWChar == PWChar.upper():
+            Criteria_02 = True
+        elif PWChar == PWChar.lower():
+            Criteria_00 = True
+    elif PWChar.isdecimal() == True:
+        Criteria_03 = True
+    elif PWChar in spcl_Char:
+        Criteria_04 = True
+    elif PWChar == Usr_Pass[15]:
+        Criteria_01 = True
+    else:
+        pass
 
 print(Criteria_00)       
+print(Criteria_01)
 print(Criteria_02)
 print(Criteria_03)
 print(Criteria_04)
