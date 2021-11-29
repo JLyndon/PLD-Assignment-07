7aeb4b93ebc9ae9da8c7db5db3e960dad59aa850
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-11-29 11:25:58 +0800

    Isolated Instructions for WordCount, Defined Function to Disregard Spaces in Between Words

--- WordCount.py
+++ WordCount.py
@@ -1,7 +1,7 @@
-#---------- CONTEXT --------------
-#Program 1:
-#Create a program that ask for a sentence as input.
-#Display the number of words, vowels and consonants in the input 
+# ---------- CONTEXT --------------
+# Program 1:
+# Create a program that ask for a sentence as input.
+# Display the number of words, vowels and consonants in the input 
 #  Example:
 #     input: Sample Text here
 #     output:
@@ -9,19 +9,39 @@
 #           vowels: 6
 #           consonants: 8
 
+def EliminateSpace_(Text):
+    sub_word_count = 0
+    identif_word, *temp = Text.split(" ") 
+    for character in identif_word:
+        if (character != "") or (character != None) or (character.isspace() == False):
+            sub_word_count = 1
+        else:
+            None
+    for sub_char in temp:
+        for lower_char in sub_char:
+            if (lower_char != "") or (sub_char != None) or (sub_char.isspace() == False):
+                sub_word_count += 1
+                break
+            else:
+                None
+    return sub_word_count
+
+
 Usr_Text = input("Enter text: ").lower()
 
-vowel_list = "a","e","i","o","u"
-word_count = 1
+word_count = EliminateSpace_(Usr_Text)
 vowels = 0
 consonants = 0
 
+vowel_str = "aeiou"
+
 for char in Usr_Text:
     if char == " ":
-        word_count += 1
-    elif char in vowel_list:
+        if (Usr_Text.isspace() == True) or ((Usr_Text == "") or (Usr_Text == None)):
+            break
+    elif char in vowel_str:
         vowels += 1
-    elif char not in vowel_list:
+    elif char not in vowel_str:
         if char.isalpha() == True:
             consonants += 1
     else:
